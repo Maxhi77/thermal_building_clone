3bfd7e2d10a739fc46cccb0a937232353110acf0
hill_mx <maximilian.hillen@dlr.de>
2025-03-17 02:01:07 +0100

    simplyfied scritp for easyer bug finding

--- src/oemof/thermal_building_model/examples/03_advanced_investment_optimization/01_multiple_heat_temp_levels.py
+++ src/oemof/thermal_building_model/examples/03_advanced_investment_optimization/01_multiple_heat_temp_levels.py
@@ -1,11 +1,14 @@
-from thermal_building_model.oemof_facades.infrastructure.grids import ElectricityGrid, HeatGrid
-from thermal_building_model.oemof_facades.infrastructure.carriers import ElectricityCarrier, HeatCarrier
-from thermal_building_model.oemof_facades.helper_functions import connect_buses, flatten_components_list
-from thermal_building_model.oemof_facades.infrastructure.demands import ElectricityDemand, HeatDemand
-from thermal_building_model.oemof_facades.technologies.renewable_energy_source import PVSystem
-from thermal_building_model.oemof_facades.technologies.storages import Battery, HotWaterTank
+from oemof.thermal_building_model.oemof_facades.infrastructure.grids import ElectricityGrid, HeatGrid
+from oemof.thermal_building_model.oemof_facades.infrastructure.carriers import ElectricityCarrier, HeatCarrier
+from oemof.thermal_building_model.oemof_facades.helper_functions import connect_buses, flatten_components_list
+from  oemof.thermal_building_model.oemof_facades.infrastructure.demands import ElectricityDemand, HeatDemand
+from  oemof.thermal_building_model.oemof_facades.technologies.renewable_energy_source import PVSystem
+from  oemof.thermal_building_model.oemof_facades.technologies.storages import Battery, HotWaterTank
 from oemof import solph
-from thermal_building_model.oemof_facades.constraints.storage_level import storage_level_constraint
+import matplotlib.pyplot as plt
+import networkx as nx
+from oemof.network.graph import create_nx_graph
+from  oemof.thermal_building_model.oemof_facades.constraints.storage_level import storage_level_constraint
 #  create solver
 solver = "gurobi"  # 'glpk', 'gurobi',....
 number_of_time_steps = 3
@@ -50,7 +53,7 @@ heat_grid_bus_into_grid = heat_grid_dataclass.get_bus_into_grid()
 
 heat_carrier_temperature_levels = [20]
 heat_carrier_dataclass = HeatCarrier(levels = heat_carrier_temperature_levels)
-#heat_carrier_dataclass.connect_buses_decreasing_levels()
+heat_carrier_dataclass.connect_buses_decreasing_levels()
 heat_carrier_bus = heat_carrier_dataclass.get_bus()
 connect_buses(
     input=heat_grid_bus_from_grid,
@@ -64,22 +67,23 @@ heat_demand_dataclass = HeatDemand(name="HotWater",
 
 
 hot_water_tank_dataclass = HotWaterTank(
+    min_temperature=min(heat_carrier_bus.keys()),
+    max_temperature=max(heat_carrier_bus.keys())+10,
     temperature_buses = heat_carrier_bus
     )
-hot_water_tank_bus_from_storage = hot_water_tank_dataclass.get_bus_from_storage()
-hot_water_tank_bus_into_storage = hot_water_tank_dataclass.get_bus_into_storage()
+hot_water_tank_bus_from_storage = hot_water_tank_dataclass.generate_bus_from_storage()
+hot_water_tank_bus_into_storage = hot_water_tank_dataclass.generate_storage_into_bus()
 hot_water_tank = hot_water_tank_dataclass.create_storage(input_bus=hot_water_tank_bus_into_storage,
-                                        output_bus=hot_water_tank_bus_from_storage)
-generate_error = True
-connect_buses(
-    input=hot_water_tank_bus_from_storage,
-              target=heat_carrier_bus,
-              #output=hot_water_tank_bus_into_storage
-    )
+                                                        output_bus=hot_water_tank_bus_from_storage)
+generate_error = False
 if generate_error:
-    connect_buses(#input = heat_carrier_bus,
-                 target=heat_carrier_bus,
-                 output=hot_water_tank_bus_into_storage)
+    connect_buses(input=heat_carrier_bus,
+                  target=hot_water_tank_bus_into_storage,
+
+        )
+
+connect_buses(input = hot_water_tank_bus_from_storage,
+             target=heat_carrier_bus)
 
 es.add(hot_water_tank_bus_from_storage,hot_water_tank_bus_into_storage,hot_water_tank)
 
@@ -92,11 +96,12 @@ if find_error_plotting:
         list.append(heat_carrier_bus[i].inputs)
     list.append(hot_water_tank_bus_into_storage.inputs)
     list.append(hot_water_tank_bus_into_storage.outputs)
-battery_dataclass = Battery(investment=True)
-battery = battery_dataclass.create_storage(input_bus = electricity_carrier_bus,
-                                           output_bus = electricity_carrier_bus,
-                                           )
-es.add(battery)
+if False:
+    battery_dataclass = Battery(investment=True)
+    battery = battery_dataclass.create_storage(input_bus = electricity_carrier_bus,
+                                               output_bus = electricity_carrier_bus,
+                                               )
+    es.add(battery)
 
 pv_dataclass = PVSystem()
 pv_system = pv_dataclass.create_source(output_bus = electricity_carrier_bus,
@@ -105,7 +110,7 @@ es.add(pv_system)
 
 heat_demand = heat_demand_dataclass.create_demand()
 heat = []
-if False:
+if True:
     heat = [heat_grid_sink,
             heat_grid_source,
             heat_grid_bus_from_grid,
@@ -113,11 +118,21 @@ if False:
             heat_carrier_bus,
             heat_demand]
 
-
-electricity_components = flatten_components_list(electricity)
+if False:
+    electricity_components = flatten_components_list(electricity)
+    es.add(*(electricity_components))
 heat_components = flatten_components_list(heat)
 # Add all components to the energy system in one go
-es.add(*(electricity_components + heat_components))
+es.add(*(heat_components))
+# Create the graph from the energy system (es)
+graph = create_nx_graph(es)
+
+# Draw the graph
+plt.figure(figsize=(10, 6))  # Set figure size
+nx.draw(graph, with_labels=True, font_size=8)
+
+# Show the graph
+plt.show()
 model = solph.Model(es)
 if False:
     storage_level_constraint(model,
