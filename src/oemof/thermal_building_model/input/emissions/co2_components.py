301ca9d3108db167c2cb32bcc8a28b738090ccfc
hill_mx <maximilian.hillen@dlr.de>
2025-03-24 21:31:07 +0100

    delete co2 components as unique data file

--- src/oemof/thermal_building_model/input/emissions/co2_components.py
+++ src/oemof/thermal_building_model/input/emissions/co2_components.py
@@ -1,20 +1,5 @@
-from oemof.thermal_building_model.oemof_facades.base_component import CO2Components
 
 
-gas_co2 = CO2Components(per_flow=0.2511 / 1000)
 
-electricity_co2 = CO2Components(per_flow=0.380 / 1000)
 
-air_heat_pump_co2 = CO2Components(per_capacity=0.03097)
-
-gas_heater_co2 = CO2Components(per_capacity=0.00809)
-
-heat_grid_co2 = CO2Components(per_capacity=0.01264,
-                              per_flow=0.1655 / 1000)
-
-hot_water_tank_co2 = CO2Components(per_capacity=0.2695)
-
-battery_co2 = CO2Components(per_capacity=0.1303)
-
-pv_system_co2 = CO2Components(per_capacity=0.91)
 
