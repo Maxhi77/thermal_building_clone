93af74a472cd29073f9bdee2f408f968b5c972b1
hill_mx <maximilian.hillen@dlr.de>
2025-04-15 19:15:25 +0200

    change input values

--- src/oemof/thermal_building_model/input/economics/investment_components.py
+++ src/oemof/thermal_building_model/input/economics/investment_components.py
@@ -11,38 +11,38 @@ battery_config = InvestmentComponents(
 )
 # capacity is m^3
 hot_water_tank_config = InvestmentComponents(
-    maximum_capacity=40,
-    minimum_capacity=1,
+    maximum_capacity=200,
+    minimum_capacity=2,
     cost_per_unit=300 / 1000,
     cost_offset=200,
     co2_per_capacity=0.2695,
-    lifetime=20,
-    operational_cost_relative_to_capacity = 0.02
+    lifetime=30,
+    operational_cost_relative_to_capacity = 0.01
 )
 
 air_heat_pump_config = InvestmentComponents(
     maximum_capacity=100000,
     minimum_capacity=2000,
-    cost_per_unit=800 / 1000,
+    cost_per_unit=1600 / 1000,
     cost_offset=6000,
     lifetime=20,
     co2_per_capacity = 0.03097,
-    operational_cost_relative_to_capacity= 0.02,
+    operational_cost_relative_to_capacity= 0.025,
 )
 
 gas_heater_config = InvestmentComponents(
     maximum_capacity=100000,
     minimum_capacity=2000,
-    cost_per_unit=100 / 1000,
-    cost_offset=2800,
-    operational_cost_relative_to_capacity=0.015,
+    cost_per_unit=300 / 1000,
+    cost_offset=3528,
+    operational_cost_relative_to_capacity=0.01,
     co2_per_capacity=0.00809,
-    lifetime=20,
+    lifetime=25,
 )
 
 pv_system_config = InvestmentComponents(
     maximum_capacity=12000,
-    cost_per_unit=800 /1000,
+    cost_per_unit=900 /1000,
     cost_offset=500,
     operational_cost_relative_to_capacity=0.02,
     co2_per_capacity=0.91,
@@ -57,4 +57,14 @@ heat_grid_config = InvestmentComponents(
     operational_cost_relative_to_capacity=0.02,
     co2_per_capacity=0.02264,
     lifetime=25,
+)
+
+chp_config = InvestmentComponents(
+    maximum_capacity=30000,
+    minimum_capacity=3000,
+    cost_per_unit=20 / 1000,
+    cost_offset=8000,
+    operational_cost_relative_to_capacity=0.02,
+    co2_per_capacity=0.02264,
+    lifetime=15,
 )
